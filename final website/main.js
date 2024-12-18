d0a7e155e46e0c0822c98a55b42bde1c43d2dffb
Mahreebmehmood <mahreebmehmood@gmail.com>
2024-12-18 16:31:54 +0500

    first commit

---
+++ final website/main.js
@@ -0,0 +1,8 @@
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Collapse });
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
